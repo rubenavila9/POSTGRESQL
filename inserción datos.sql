f7eaac1503842583a3c5b3190406faa3c00bae9d
rubenavila9 <56893148+rubenavila9@users.noreply.github.com>
2020-08-27 19:57:26 -0500

    Create inserción datos.sql

---
+++ inserción datos.sql
@@ -0,0 +1,31 @@
+insert into CLIENTE values
+(123242424, 'Ruben','Avila','barrio cuba', '09686389390','ruben@gmail.com'),
+(128883913, 'deiby','vera','barrio altamira', '0938173823','deiby@gmail.com'),
+(113233142, 'ricardo','lugo','barrio cuba', '09839281992','ricardo@gmail.com'),
+(131313313, 'ariel','zambrano','barrio cuba', '0917389283','ariel@gmail.com'),
+
+select * from CLIENTE;
+
+insert into PACIENTE values
+(1, 'benji','perro','cocker', 'masculino','3 años','ninguna','rabia',123242424),
+(2, 'pelussa','perro','mestiza', 'femenino','2 años','ninguna','rabia',128883913),
+(3, 'roco','perro','labrador', 'masculino','1 años','ninguna','rabia',113233142),
+(4, 'misi','gato','persa', 'masculino','6 años','ninguna','rabia',123242424)
+
+select * from PACIENTE;
+
+insert into PESO values
+(1,'10/8/2020','10kg'),
+(2,'4/8/2020','5kg'),
+(3,'5/8/2020','6kg'),
+(4,'9/8/2020','3kg'),
+
+select * from PESO;
+
+insert into VACUNA values
+(1,'10/8/2020','parasitaria','20/10/2020'),
+(2,'12/8/2020','rabia','18/10/2020'),
+(3,'16/8/2020','vitaminas','5/10/2020'),
+(4,'20/8/2020','parasitaria','3/10/2020'),
+
+select * from PESO;
